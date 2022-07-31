5e05a21cbc7fe6e122af696c767f7dc0cf121335
kouwasi <kouwasi@gmail.com>
2022-07-31 22:24:54 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Nuxt 2 composition api circular issue with setup syntax sugar
+
+The repository for reproducing to composition api circular issue.
+
+At first, run the dev server and open `pages/index.vue` files. you'll see the issue!
+
+## Build Setup
+
+```bash
+# install dependencies
+$ yarn install
+
+# serve with hot reload at localhost:3000
+$ yarn dev
+```
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <span @click="goDetail">{{ zen }}</span>
+</template>
+
+<script setup>
+import { ref, useContext, useFetch, useRouter } from "@nuxtjs/composition-api"
+
+const { $axios } = useContext();
+const zen = ref("")
+
+useFetch(async () => {
+  const response = await $axios.get("https://api.github.com/zen")
+  zen.value = response.data
+})
+
+// Commnet out belows to works!!!
+const router = useRouter()
+const goDetail = () => {
+  router.push('/detail')
+}
+</script>
